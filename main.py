13c30db5f378f08ac2ad8c0f2866f37ffcd56e9a
Vladislav <vladislav.krasivin@gmail.com>
2023-12-22 15:26:05 +0300

    Добавлены базовые сигнатуры функций

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import parser_utils as parser
 def main():
     driver = parser.create_driver()
     driver.get('https://duckduckgo.com/')
-    #
+    # вызов get_data()
     parser.kill_driver(driver)
 
 
